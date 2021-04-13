fc4ec6faf59ffe3e03b3cd91a6c0c96f1b96083a
ksted <kevin.stederoth@plentymarkets.com>
2021-04-13 14:23:45 +0200

    add error response example

---
+++ docs/en-gb/modules/ROOT/examples/sdk-error-response.php
@@ -0,0 +1,14 @@
+array(6) {
+  'error' =>
+    bool(true)
+  'error_no' =>
+    int(404)
+  'error_msg' =>
+    string(10) "error msg"
+  'error_file' =>
+    string(10) "/path/to/lib/file"
+  'error_line' =>
+    int(11)
+  'lib_call_response' =>
+    NULL
+}
